185b77b88deabef1dde46c8d2d541053330f4b11
Adit Gupta <aditgupta@gmail.com>
2019-10-09 20:55:07 +0530

    Update results.py

--- results.py
+++ results.py
@@ -13,7 +13,7 @@ while True:
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     response = requests.get(url, headers=headers)
-    # parse the downloaded homepage and grab all text
+    
     soup = BeautifulSoup(response.text, "lxml")
 
 
@@ -32,20 +32,20 @@ while True:
 
         toaddrs = ['<email address>', '<another email address>']
 
-        # setup the email server,
+        
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        # add my account login name and password,
+       
         server.login("<from email address>", "PutYourOwnPassWordDude")
 
-        # Print the email's contents
+        
         print('From: ' + fromaddr)
         print('To: ' + str(toaddrs))
         print('Message: ' + msg)
 
-        # send the email
+        
         server.sendmail(fromaddr, toaddrs, msg)
-        # disconnect from the server
+        
         server.quit()
 
         break
